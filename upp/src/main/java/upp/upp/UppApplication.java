b2d6012dbb32ee084512585aa2c55a2d85b14354
Vladimir <vladimir94@gmail.com>
2018-02-11 22:09:17 +0100

    init

---
+++ upp/src/main/java/upp/upp/UppApplication.java
@@ -0,0 +1,12 @@
+package upp.upp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppApplication.class, args);
+	}
+}
